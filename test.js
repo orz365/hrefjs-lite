3303cea6776462d99b88caf8df0cbdb0897da9b0
亡命 <orz365@163.com>
2020-12-10 10:59:20 +0800

    格式化代码

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-const hrefjs = require('./src/href')
-var location = hrefjs('https://github.com/MikeMcl/bignumber.js/blob/master/bignumber.js')
-console.log(location)
+const hrefjs = require('./src/href');
+var location = hrefjs('https://github.com/MikeMcl/bignumber.js/blob/master/bignumber.js');
+console.log(location);
 
 var json = {
-    a: 10,
-    b: 20
-}
-var param = hrefjs.json2param(json)
+	a: 10,
+	b: 20
+};
+var param = hrefjs.json2param(json);
 
-parma = 'a=10&b=20'
-json = hrefjs.param2json(param)
-console.log(json)
+parma = 'a=10&b=20';
+json = hrefjs.param2json(param);
+console.log(json);
